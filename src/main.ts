461117a190a5551d67a4831240cd19ca24314b8b
Ramon Molinero <molinero.gomez.ramon@gmail.com>
2025-01-12 14:26:12 +0100

    openApi start config (Swagger)

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  
   app.useGlobalPipes(
     new ValidationPipe({
-        whitelist: true,
-        forbidNonWhitelisted: true,
-      })
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    })
   );
-
+  
   app.setGlobalPrefix('api'); // 👈
+  
+  const config = new DocumentBuilder()
+    .setTitle('API TesloShop')
+    .setDescription('Documentación de la API para TesloShop')
+    .setVersion('1.0')
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
 
   await app.listen(process.env.PORT ?? 3000);
 }
