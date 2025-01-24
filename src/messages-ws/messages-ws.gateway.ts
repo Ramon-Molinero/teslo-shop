91204b80f9dbded6fad42927e013e5692ecc36a5
Ramon Molinero <molinero.gomez.ramon@gmail.com>
2025-01-24 05:22:53 +0100

    Jwt connexion + device validation

--- src/messages-ws/messages-ws.gateway.ts
+++ src/messages-ws/messages-ws.gateway.ts
@@ -2,25 +2,53 @@ import { OnGatewayConnection, OnGatewayDisconnect, SubscribeMessage, WebSocketGa
 import { MessagesWsService } from './messages-ws.service';
 import { Server, Socket } from 'socket.io';
 import { WsMessage } from './dtos/ws-message';
+import { JwtService } from '@nestjs/jwt';
+import { JwtPayload } from '../auth/interfaces';
+import { UAParser } from 'ua-parser-js';
 
 
 @WebSocketGateway({ cors: true})
 export class MessagesWsGateway implements OnGatewayConnection, OnGatewayDisconnect {
   
+  private readonly uaParser = new UAParser();
+
   @WebSocketServer() wss: Server;
-  constructor(private readonly messagesWsService: MessagesWsService) {}
+  constructor(
+    private readonly messagesWsService: MessagesWsService,
+    private readonly jwtService: JwtService
+  ) {}
 
 
-  handleConnection(client: Socket) {
-    this.messagesWsService.registerClient(client);
+  async handleConnection(client: Socket) {
+
+    const token = client.handshake.headers.authorization as string;
+    const deviceType = this.connexionDeviceType(client);
+    let payload: JwtPayload
+
+
+    try {
+      payload = this.jwtService.verify(token);
+      await this.messagesWsService.registerClient(client, payload.id, deviceType);
+      
+    } catch (error) {
+      client.disconnect();
+      return;
+    }
+    
     this.wss.emit('clients-updated', this.messagesWsService.getConnectedClients());
+    
+    
   }
+
 
 
   handleDisconnect(client: Socket) {
+
     this.messagesWsService.removeClient(client);
     this.wss.emit('clients-updated', this.messagesWsService.getConnectedClients());
   }
+
+
 
   @SubscribeMessage('message-form-client')
     onMessageFormClient(client: Socket, payload: WsMessage){
@@ -39,10 +67,31 @@ export class MessagesWsGateway implements OnGatewayConnection, OnGatewayDisconne
 
       //! Emitir a todos los clientes conectados
       this.wss.emit('message-from-server', {
-        fullName: 'yo',
+        fullName: this.messagesWsService.getUserFullNameBySocketId(client.id),
         message: payload.message || 'no message',
       });
         
+    }
+
+    connexionDeviceType(client: Socket): 'mobile' | 'tablet' | 'desktop' {
+      const userAgent: string = client.handshake.headers['user-agent'] || '';
+      const deviceType = this.detectDevice(userAgent);
+
+      return deviceType;
+    }
+
+    private detectDevice(userAgent: string): 'mobile' | 'tablet' | 'desktop' {
+      const device = this.uaParser.setUA(userAgent).getDevice();
+
+      switch (device.type) {
+        case 'mobile':
+          return 'mobile';
+        case 'tablet':
+          return 'tablet';
+        default:
+          return 'desktop';
+      }
+
     }
 
 
--- src/messages-ws/messages-ws.service.ts
+++ src/messages-ws/messages-ws.service.ts
@@ -1,27 +1,111 @@
 import { Injectable } from '@nestjs/common';
 import { Socket } from 'socket.io';
+import { User } from '../auth/entities/user.entity';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
 
-
-interface ConnectedClients{
-    [id: string]: Socket;
+interface ConnectedClients {
+    [id: string]: {
+        socket: Socket;
+        user: User;
+        mobile: boolean;
+        tablet: boolean;
+        desktop: boolean;
+    };
 }
 
 @Injectable()
 export class MessagesWsService {
-
     private connectedClients: ConnectedClients = {};
 
-    registerClient ( client: Socket){
-        this.connectedClients[client.id] = client;
+    constructor(
+        @InjectRepository(User)
+        private readonly userRepository: Repository<User>,
+    ) { }
+
+    
+    async registerClient(
+        client: Socket,
+        userId: string,
+        deviceType: 'mobile' | 'tablet' | 'desktop',
+    ) {
+        try {
+            const user = await this.userRepository.findOneBy({ id: userId });
+
+            if (!user) throw new Error('User not found');
+            if (!user.isActive) throw new Error('User is not active');
+
+            const socketObject = this.generateObjetByconectedClients(client, user, deviceType);
+
+            await this.checkUserConnection(socketObject, deviceType);
+
+            this.connectedClients[client.id] = socketObject;
+
+
+        } catch (error) {
+            console.error('Error registering client:', error.message);
+        }
     }
 
-    removeClient ( client: Socket){
+
+    removeClient(client: Socket) {
         delete this.connectedClients[client.id];
     }
 
-    getConnectedClients(): string[]{
+
+    getConnectedClients(): string[] {
+
         return Object.keys(this.connectedClients);
     }
 
-    
+
+    getUserFullNameBySocketId(socketId: string): string {
+        return this.connectedClients[socketId].user.fullName;
+    }
+
+
+    private generateObjetByconectedClients(
+        client: Socket,
+        user: User,
+        deviceType: 'mobile' | 'tablet' | 'desktop',
+    ) {
+        const socketObject = {
+            socket: client,
+            user,
+            mobile: deviceType === 'mobile' ? true : false,
+            tablet: deviceType === 'tablet' ? true : false,
+            desktop: deviceType === 'desktop' ? true : false,
+        };
+
+        return socketObject;
+    }
+
+
+    private checkUserConnection(socketObject: any, deviceType: 'mobile' | 'tablet' | 'desktop') {
+
+
+        for (const clientId of Object.keys(this.connectedClients)) {
+
+            const connectedClient = this.connectedClients[clientId];
+
+            if (connectedClient.user.id === socketObject.user.id) {
+
+                if (
+                    (connectedClient.mobile && deviceType === 'mobile') ||
+                    (connectedClient.tablet && deviceType === 'tablet') ||
+                    (connectedClient.desktop && deviceType === 'desktop')
+                ) {
+                    console.log(
+                        `Disconnecting existing connection for user ${socketObject.user.id} on device type ${deviceType}`
+                    );
+                    connectedClient.socket.disconnect();
+                    delete this.connectedClients[clientId];
+                    break;
+                }
+            }
+        }
+
+    }
+
+
 }
