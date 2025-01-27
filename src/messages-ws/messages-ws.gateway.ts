ebce0c080404d3ac391c73bde813304329987a4d
Ramon Molinero <molinero.gomez.ramon@gmail.com>
2025-01-27 12:47:18 +0100

    add comments

--- src/messages-ws/messages-ws.gateway.ts
+++ src/messages-ws/messages-ws.gateway.ts
@@ -7,11 +7,35 @@ import { JwtPayload } from '../auth/interfaces';
 import { UAParser } from 'ua-parser-js';
 
 
+
+
+/**
+  * @class MessagesWsGateway
+  * 
+  * @author R.M
+  * @version 1.0
+  *
+  * @description
+  * Este gateway gestiona las conexiones y desconexiones de clientes mediante WebSocket, así como el envío y recepción de mensajes en tiempo real. 
+  * Implementa las interfaces `OnGatewayConnection` y `OnGatewayDisconnect` para controlar la conexión y desconexión de clientes. 
+  * También identifica el tipo de dispositivo (móvil, tableta o escritorio) que realiza la conexión.
+  *
+  * @decorator `@WebSocketGateway({ cors: true })`
+  * Habilita un servidor WebSocket con soporte para CORS.
+  *
+  */
 @WebSocketGateway({ cors: true})
 export class MessagesWsGateway implements OnGatewayConnection, OnGatewayDisconnect {
   
   private readonly uaParser = new UAParser();
 
+
+  /**
+    * @property {Server} wss
+    * Instancia del servidor WebSocket para gestionar y emitir eventos a los clientes conectados.
+    * 
+    * @decorator `@WebSocketServer`
+    */
   @WebSocketServer() wss: Server;
   constructor(
     private readonly messagesWsService: MessagesWsService,
@@ -19,10 +43,24 @@ export class MessagesWsGateway implements OnGatewayConnection, OnGatewayDisconne
   ) {}
 
 
+  /**
+    * @method handleConnection
+    * 
+    * @author R.M
+    * @version 1.0
+    *
+    * @description
+    * Maneja la conexión de un cliente al servidor WebSocket.
+    * Valida el token JWT del cliente, registra al cliente en el sistema, y emite un evento global con la lista actualizada de clientes conectados.
+    *
+    * @param {Socket} client - Instancia del cliente conectado.
+    * 
+    * @throws {Error} Desconecta al cliente si el token JWT es inválido o si ocurre algún error durante la conexión.
+   */
   async handleConnection(client: Socket) {
 
     const token = client.handshake.headers.authorization as string;
-    const deviceType = this.connexionDeviceType(client);
+    const deviceType = this._connexionDeviceType(client);
     let payload: JwtPayload
 
 
@@ -42,6 +80,17 @@ export class MessagesWsGateway implements OnGatewayConnection, OnGatewayDisconne
 
 
 
+  /**
+   * @method handleDisconnect
+   * 
+   * @author R.M
+   * @version 1.0
+   *
+   * @description
+   * Maneja la desconexión de un cliente, eliminándolo del registro de clientes conectados y notificando a los demás clientes.
+   *
+   * @param {Socket} client - Instancia del cliente desconectado.
+   */
   handleDisconnect(client: Socket) {
 
     this.messagesWsService.removeClient(client);
@@ -50,6 +99,17 @@ export class MessagesWsGateway implements OnGatewayConnection, OnGatewayDisconne
 
 
 
+  /**
+    * @method onMessageFormClient
+    *
+    * @description
+    * Escucha los mensajes enviados por un cliente y los retransmite a todos los clientes conectados mediante el evento `message-from-server`.
+    *
+    * @decorator `@SubscribeMessage('message-form-client')`
+    * 
+    * @param {Socket} client - Cliente que envió el mensaje.
+    * @param {WsMessage} payload - Contenido del mensaje enviado.
+    */
   @SubscribeMessage('message-form-client')
     onMessageFormClient(client: Socket, payload: WsMessage){
       
@@ -73,14 +133,44 @@ export class MessagesWsGateway implements OnGatewayConnection, OnGatewayDisconne
         
     }
 
-    connexionDeviceType(client: Socket): 'mobile' | 'tablet' | 'desktop' {
+
+    /**
+      * @method _connexionDeviceType
+      * 
+      * @author R.M
+      * @version 1.0
+      * 
+      * @private
+      * 
+      * @description
+      * Determina el tipo de dispositivo que realiza la conexión basándose en el encabezado `User-Agent`.
+      * 
+      * @param {Socket} client - Instancia del cliente conectado.
+      * @returns {'mobile' | 'tablet' | 'desktop'} - Tipo de dispositivo detectado.
+      */
+    private _connexionDeviceType(client: Socket): 'mobile' | 'tablet' | 'desktop' {
       const userAgent: string = client.handshake.headers['user-agent'] || '';
-      const deviceType = this.detectDevice(userAgent);
+      const deviceType = this._detectDevice(userAgent);
 
       return deviceType;
     }
 
-    private detectDevice(userAgent: string): 'mobile' | 'tablet' | 'desktop' {
+
+    /**
+      * @method _detectDevice
+      * 
+      * @author R.M
+      * @version 1.0
+      * 
+      * @private
+      * 
+      * @description
+      * Analiza el `User-Agent` proporcionado para identificar el tipo de dispositivo.
+      * 
+      * @param {string} userAgent - Cadena del `User-Agent` del cliente.
+      * @returns {'mobile' | 'tablet' | 'desktop'} - Tipo de dispositivo detectado.
+      */
+    private _detectDevice(userAgent: string): 'mobile' | 'tablet' | 'desktop' {
       const device = this.uaParser.setUA(userAgent).getDevice();
 
       switch (device.type) {
--- src/messages-ws/messages-ws.service.ts
+++ src/messages-ws/messages-ws.service.ts
@@ -18,12 +18,52 @@ interface ConnectedClients {
 export class MessagesWsService {
     private connectedClients: ConnectedClients = {};
 
+
+    /**
+      * @constructor
+      * 
+      * @description
+      * Constructor de la clase que utiliza la inyección de dependencias para inicializar el repositorio de usuarios.
+      * Este repositorio se utiliza para interactuar con la base de datos y realizar operaciones relacionadas con la entidad `User`.
+      * 
+      * @param {Repository<User>} userRepository - Repositorio de TypeORM para la entidad `User`.
+      * 
+      * @decorator `@InjectRepository(User)`
+      * Inyecta automáticamente el repositorio asociado a la entidad `User` en la clase.
+      */
     constructor(
         @InjectRepository(User)
         private readonly userRepository: Repository<User>,
     ) { }
 
-    
+
+
+    /**
+      * @author R.M
+      * @version 1.0
+      * @method registerClient
+      *
+      * @description
+      * Registra un cliente en el sistema de WebSocket, asociándolo a un usuario específico y verificando si el usuario ya tiene una conexión activa en el mismo tipo de dispositivo.
+      * 
+      * ### Proceso:
+      * 1. **Buscar Usuario**: Consulta la base de datos para encontrar al usuario asociado al `userId` proporcionado.
+      * 2. **Validar Usuario**:
+      *    - Lanza un error si el usuario no existe.
+      *    - Lanza un error si el usuario no está activo.
+      * 3. **Generar Objeto de Conexión**:
+      *    - Crea un objeto que contiene la información del cliente, el usuario y el tipo de dispositivo.
+      * 4. **Verificar Conexión Activa**:
+      *    - Llama a `checkUserConnection` para desconectar cualquier conexión activa existente del usuario en el mismo tipo de dispositivo.
+      * 5. **Registrar Cliente**:
+      *    - Agrega el cliente al listado de clientes conectados (`connectedClients`).
+      * 
+      * @param {Socket} client - Instancia del socket del cliente conectado.
+      * @param {string} userId - Identificador único del usuario que se conecta.
+      * @param {'mobile' | 'tablet' | 'desktop'} deviceType - Tipo de dispositivo desde el que se realiza la conexión.
+      * 
+      * @throws {Error} Si el usuario no existe o no está activo.
+      */
     async registerClient(
         client: Socket,
         userId: string,
@@ -48,22 +88,69 @@ export class MessagesWsService {
     }
 
 
+    /**
+      * @method removeClient
+      * 
+      * @author R.M
+      * @version 1.0
+      * 
+      * @description
+      * Elimina un cliente del sistema de WebSocket, desconectándolo del servidor.
+      * 
+      * @param {Socket} client - Cliente que se desconecta.
+      */
     removeClient(client: Socket) {
         delete this.connectedClients[client.id];
     }
 
 
+    /**
+      * @method getConnectedClients
+      * 
+      * @author R.M
+      * @version 1.0
+      * 
+      * @description
+      * Devuelve un array con los identificadores de los clientes conectados al servidor.
+      * 
+      * @returns {string[]} Array con los identificadores de los clientes conectados.
+      */
     getConnectedClients(): string[] {
 
         return Object.keys(this.connectedClients);
     }
 
 
+    /**
+      * @method getUserFullNameBySocketId
+      * 
+      * @author R.M
+      * @version 1.0
+      * 
+      * @description
+      * Devuelve el nombre completo del usuario asociado a un identificador de socket.
+      * 
+      * @param {string} socketId - Identificador del socket del cliente.
+      * 
+      * @returns {string} Nombre completo del usuario.
+      */
     getUserFullNameBySocketId(socketId: string): string {
         return this.connectedClients[socketId].user.fullName;
     }
 
 
+    /**
+      * @method sendMessageToUser
+      * 
+      * @author R.M
+      * @version 1.0
+      * 
+      * @description
+      * Envía un mensaje a un usuario específico, utilizando el identificador del socket del cliente.
+      * 
+      * @param {string} socketId - Identificador del socket del cliente.
+      * @param {string} message - Mensaje a enviar.
+      */
     private generateObjetByconectedClients(
         client: Socket,
         user: User,
@@ -72,15 +159,26 @@ export class MessagesWsService {
         const socketObject = {
             socket: client,
             user,
-            mobile: deviceType === 'mobile' ? true : false,
-            tablet: deviceType === 'tablet' ? true : false,
-            desktop: deviceType === 'desktop' ? true : false,
+            mobile: deviceType === 'mobile',
+            tablet: deviceType === 'tablet',
+            desktop: deviceType === 'desktop',
         };
-
+        
         return socketObject;
     }
 
 
+    /**
+      * @method checkUserConnection
+      * @author R.M
+      * @version 1.0
+      * 
+      * @description
+      * Verifica si un usuario ya tiene una conexión activa en el mismo tipo de dispositivo y desconecta la conexión existente.
+      * 
+      * @param {any} socketObject - Objeto que contiene la información del cliente, el usuario y el tipo de dispositivo.
+      * @param {'mobile' | 'tablet' | 'desktop'} deviceType - Tipo de dispositivo desde el que se realiza la conexión.
+      */
     private checkUserConnection(socketObject: any, deviceType: 'mobile' | 'tablet' | 'desktop') {
 
 
